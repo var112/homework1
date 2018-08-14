eb87ca9ce7e81cf02bd571677bb09a4be5fc8d17
var112 <42339879+var112@users.noreply.github.com>
2018-08-14 14:00:39 +0300

    Окунь

---
+++ script.js
@@ -0,0 +1,32 @@
+var string = " The quick brown fox quickly jumps over the lazy dog. ",
+    modification;
+
+string[0] + string[1]
+
+string.indexOf("quick")
+
+string.indexOf("Quick")
+
+string.lastIndexOf("quick")
+
+modification = string.replace("quick", "slow");
+string
+
+modification
+
+var splitted = string.split(" ");
+splitted
+
+splitted.join("-")
+
+var start = 17, lenght = 3;
+string.substr(start, lenght)
+
+string.toLowerCase()
+
+string.toUpperCase()
+
+modification = string.trim();
+string
+
+modification
